8471c2b92b43d381454b97ccc1e6196048391c74
Denno2531 <81872675+Denno2531@users.noreply.github.com>
2023-12-20 09:59:43 -0500

    Conexion

---
+++ v4/Sistema_LI/modules/conexion.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set('America/Guayaquil');
+
+$conexion = mysqli_connect("localhost", "root", "Root2023", "db_li");
+
+if (mysqli_connect_errno()) {
+	printf("Falló la conexión a la base de datos: %s\n", mysqli_connect_error());
+	exit();
+}
+
+mysqli_set_charset($conexion, 'utf8');
