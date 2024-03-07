5f7d307e59940ebc27e5dadf5145d957d212440c
ssegovia00 <sasegovia1@espe.edu.ec>
2024-03-07 12:14:54 -0500

    Correcion de visualizacion en la tabla de justificaciones en editor

--- v4/Sistema_LI/modules/edit_send_one/form_justificaciones.php
+++ v4/Sistema_LI/modules/edit_send_one/form_justificaciones.php
@@ -2,126 +2,86 @@
 require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php');
 include_once '../conexion.php';
 
-$sql = "SELECT * FROM justificaciones";
+$max = 10;
 
-if ($result = $conexion->query($sql)) {
-	if ($row = mysqli_fetch_array($result)) {
-	  $_SESSION['send_estado'] = $row['estado'];
-	  $_SESSION['send_created'] = $row['created_at'];
-	  $_SESSION['send_updated'] = $row['updated_at'];
-	}
-  }
-// Debes definir el valor de $max antes de usarlo en el cálculo de $tpages
-$max = 10; // Aquí debes proporcionar el valor apropiado
-
-$sql = "SELECT COUNT(num) AS total FROM justificaciones";
-
-if ($result = $conexion->query($sql)) {
-	if ($row = mysqli_fetch_array($result)) {
-		$tpages = ceil($row['total'] / $max);
-	}
-}
-
-if (!empty($_POST['search'])) {
-	$_POST['search'] = trim($_POST['search']);
-	$_POST['search'] = mysqli_real_escape_string($conexion, $_POST['search']);
-
-	$_SESSION['num'] = array();
-	$_SESSION['send_archivo'] = array();
-	$_SESSION['send_estado'] = array();
-	$_SESSION['send_created'] = array();
-	$_SESSION['send_updated'] = array();
-
-	$i = 0;
-
+if ( !empty($_POST['search'])){
+	$search = mysqli_real_escape_string($conexion, $_POST['search']);
+	
+	// Consuta SQL para buscar el termino en varias columnas
 	$sql = "SELECT * FROM justificaciones WHERE num LIKE '%" . $_POST['search'] . "%' OR archivo LIKE '%" . $_POST['search'] . "%' OR user LIKE '%" . $_POST['search'] . "%' OR descripcion LIKE '%" . $_POST['search'] . "%' ORDER BY num";
-
-	if ($result = $conexion->query($sql)) {
-		while ($row = mysqli_fetch_array($result)) {
-										   
-			$_SESSION['num'][$i] = $row['num'];
-			$_SESSION['send_archivo'][$i] = $row['archivopdf'];
-			$_SESSION['send_estado'][$i] = $row['estado'];
-			$_SESSION['send_created'][$i] = $row['created_at'];
-			$_SESSION['send_updated'][$i] = $row['updated_at'];
-
-			$i += 1;
-		}
+	
+	$_result = mysqli_query($conexion, $sql);
+	
+	$_SESSION['justificaciones_data'] = [];
+	
+	// Alamacenar los resultados en el arreglo de sesión
+	while ($row = mysqli_fetch_array($result)){
+		$_SESSION['justificaciones_data'][] = $row;
 	}
-	$_SESSION['total_send'] = count($_SESSION['num']);
-} else {
-								
-	$_SESSION['num'] = array();
-	$_SESSION['send_archivo'] = array();
-
-	$i = 0;
-
-	$sql = "SELECT * FROM justificaciones WHERE user = '" . $_POST['txtuserid'] . "' ORDER BY num";
-
-	if ($result = $conexion->query($sql)) {
-		while ($row = mysqli_fetch_array($result)) {
-										   
-			$_SESSION['num'][$i] = $row['num'];
-			$_SESSION['send_archivo'][$i] = $row['archivopdf'];
-
-			$i += 1;
-		}
+	
+	$_SESSION['total_justificaciones'] = count($_SESSION['justificaciones_data']);
+}elseif (!empty($_POST['txtuserid'])) {
+	$userid = mysqli_real_escape_string($conexion, $_POST['txtuserid']);
+	
+	// Consulta SQL 
+	$sql = "SELECT * FROM justificaciones WHERE user='$userid' ORDER BY num LIMIT $max";
+	
+	$result = mysqli_query($conexion, $sql);
+	
+	$_SESSION['justificaciones_data'] = []; // array para almacenar los datos de las justificaciones encontradas
+	
+	// Alamacenar los resultados en el arreglo de sesión
+	while ($row = mysqli_fetch_array($result)) {
+		$_SESSION['justificaciones_data'][] = $row;
 	}
-	$_SESSION['total_send'] = count($_SESSION['num']);
+	
+	$_SESSION['total_justificaciones'] = count($_SESSION['justificaciones_data']);
 }
+
 ?>
 
-
-
-<div class="form-gridview">
+<div class= "form-gridview">
 	<table class="default">
-	<h2 class="titlecenter"> Justificación </h2>
-		<?php 
-			echo '<h2 class="textList"> ' .$_POST['txtname'].' </h2>'
+		<h2 class="titlecenter"> justificaciones </h2>
+		<?php
+		echo '<h2 class= "textList">' . $_POST['txtname']. '</h2'
 		?>
 		<?php
-															  
-	 
-	   
-		if ($_SESSION['total_send'] != 0) {
-			echo '
-					<tr>
-						<th class="center" style="width: 800px">Nombre del archivo</th>
-						<th class="center" style="width: 70px">Estado</th>
-						<th class="center" style="width: 300px">Creado</th>
-						<th class="center" style="width: 300px">Actualizado</th>
+		if($_SESSION['total_justificaciones'] !=0) {
 			
-				        <th class="center"><a class="icon">download</a></th>
-								<th class="center"><a class="icon">edit</a></th>
-								
-						
-			';
-			if ($_SESSION['permissions'] != 'edit') {
-				//echo '<th class="center"><a class="icon">delete</a></th>';
-			}
-			echo '	
-					</tr>
-			';
-		}	
-
-			$path = '../Justificaciones/justificacionespdf/' . $_POST["txtuserid"];
-                if(file_exists($path)){
-                    $directorio= opendir($path);
-                    while($archivo=readdir($directorio)){
-                        if(!is_dir($archivo)){
-                    	echo '
+			echo '
+				<tr>
+					<th class="center" style="width: 800px">Nombre del archivo</th>
+					<th class="center" style="width: 70px">Estado</th>
+					<th class="center" style="width: 300px">Creado</th>
+					<th class="center" style="width: 300px">Actualizado</th>
+					
+					<th class="center"><a class="icon">download</a></th>
+					<th class="center"><a class="icon">edit</a></th>
+				';
+				if($_SESSION['permission'] != 'edit'){
+					// echo 'th class="center"><a class="icon">delete</th>';
+				}
+				echo '<tr>';
+					
+				foreach($_SESSION['justificaciones_data'] as $row) {
+					$path = '../Justificaciones/justificacionespdf/' . $row['user'];
+					$archivo = $row['archivopdf'];
+					echo'
 						<tr>
 							<td>' . $archivo . '</td>
-							<td>' . $_SESSION["send_description"] . '</td>    
-							<td> 
+							<td>' . $row['estado'] . '</td>
+							<td>' . $row['created_at'] . '</td>
+							<td>' . $row['updated_at'] . '</td>
+							<td>
 								<div data="' . $path . '/' . $archivo . '"><a href="' . $path . '/' . $archivo . '"
-								title="Ver archivo adjunto" class="btnview" target="_blank"><button class="btnview" 
-								name="btn" value="form_consult" type="submit"></button></td>
+									title="Ver archivo adjunto" class="btnview" target="_blank"><button class="btnview" 
+									name="btn" value="form_consult" type="submit"></button></td>
 							<td>
 								<form action="" method="POST">
 									<input style="display:none;" type="text" name="txtuserid" value="' . $archivo . '"/>
 									<input style="display:none;" type="text" name="txtevidencefile" value="' . $_SESSION['evidencia'] . '"/>
-									<button class="btnedit" name="btn" value="form_view" type="submit"></button>
+									<button class="btnedit" name="btn" value="form_updatejustificacion" type="submit"></button>
 								</form>
 							</td>
 							<td>
@@ -132,21 +92,20 @@ if (!empty($_POST['search'])) {
 								</form>
 							</td>
 						</tr>';       
-                        }
-                    }
-                }
-
-
+				}
+		}
 		?>
 	</table>
-<br></br>
-
-   
+	<?php
+	if($_SESSION['total_justificaciones'] == 0) {
+		echo '<img src="/images/404.svg" class="data-not-found" alt="404">';
+	}
+	?>
 </div>
 
 <div class="content-aside">
-	<?php
-	include_once '../notif_info.php';
-	include_once "../sections/options-disabled.php";
-	?>
+    <?php
+    include_once '../notif_info.php';
+    include_once "../sections/options-disabled.php";
+    ?>
 </div>
