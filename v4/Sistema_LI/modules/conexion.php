1c94726e0515ac4d766242cc304ffdc9c06e0b8e
Luis-Sani <135286261+Luis-Sani@users.noreply.github.com>
2024-04-23 09:31:22 -0500

    cambio de cibntraseña bbd

--- v4/Sistema_LI/modules/conexion.php
+++ v4/Sistema_LI/modules/conexion.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/Guayaquil');
 
-$conexion = mysqli_connect("localhost", "root", "00713", "db_li");
+$conexion = mysqli_connect("localhost", "root", "admin", "db_li");
 
 if (mysqli_connect_errno()) {
 	printf("Falló la conexión a la base de datos: %s\n", mysqli_connect_error());
--- v4/Sistema_LI/modules/tutoriales_editores/index.php
+++ v4/Sistema_LI/modules/tutoriales_editores/index.php
@@ -70,7 +70,19 @@ header('Pragma: no-cache');
 	<section class="content-xl">
         <div class="container-tutorial">
 			<div class="video-container">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/okrYMAiwAKA?si=0LRHGp8nPzrYrrH9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+				<iframe width="560" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+			</div></br>
+    
+
+            
+			<div class="video-container">				
+                <video width="640" height="360" controls>
+                    <source src=".mp4" type="video/mp4">
+                </video>
+			</div></br>
+			
+			<div class="video-container">
+			
 			</div></br>
         </div>
     </section>
